cf3ffec2e24dd10db4cf422bc57d52c92f8b6440
luccas-specht <luccasspecht70@gmail.com>
2024-03-20 23:21:03 -0300

    feat: add list projects option

--- programacao1/avaliacaoFinal2/src/action/ListProjects.java
+++ programacao1/avaliacaoFinal2/src/action/ListProjects.java
@@ -1,11 +1,52 @@
 package action;
 
+import domain.IAppProject;
+import domain.IWebProject;
 import useCase.FakeDatabase;
 
+import java.util.List;
+
 public class ListProjects implements IAction {
+    private List<IAppProject> appProjects = FakeDatabase.getAppProjects();
+    private List<IWebProject> webProjects = FakeDatabase.getWebProjects();
+
+    private void printAppProjects() {
+        appProjects.forEach(element -> {
+            System.out.println(
+                    "Projeto: <" + element.getName() + "> - <" + element.getLevel() + ">\n" +
+                    "Custo: <" + element.getCost() + ">\n" +
+                    "Gerente de Projetos: <" + element.getManager() + ">\n"+
+                    "Tamanho do projeto: <" + element.getTamanho() + ">\n"+
+                    "Plataforma do projeto: <" + element.getPlataforma() + ">\n");
+        });
+    }
+
+    private void printWebProjects() {
+        webProjects.forEach(element -> {
+            System.out.println(
+                    "Projeto: <" + element.getName() + "> - <" + element.getLevel() + ">\n" +
+                    "Custo: <" + element.getCost() + ">\n" +
+                    "Gerente de Projetos: <" + element.getManager() + ">\n"+
+                    "Linguagem de programação: <" + element.getProgrammingLanguage() + ">\n"+
+                    "Banco de dados: <" + element.getDb() + ">\n");
+        });
+    }
+
+    private void checkIfThereAreProjectsToPrint() {
+        if (!appProjects.isEmpty() || !webProjects.isEmpty()) {
+            System.out.println("<vvvvvvvv Lista de projetos cadastrados: vvvvvvvv>\n");
+            printAppProjects();
+            printWebProjects();
+            System.out.println("<^^^^^^^^ Lista de projetos cadastrados: ^^^^^^^^>\n");
+        } else {
+            System.out.println("<--------- Sem projetos: ---------->\n");
+        }
+    }
+
     @Override
     public void run() {
-         FakeDatabase.getAppProjects();
-         FakeDatabase.getWebProjects();
+
+        checkIfThereAreProjectsToPrint();
+
     }
 }
